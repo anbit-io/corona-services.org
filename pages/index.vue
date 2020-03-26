548706f314f672739ba875f1ad4e29c67ed8c6df
Basant Besra <besrabasant@gmail.com>
2020-03-27 00:53:07 +0530

    Add basic coming soon

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <section
-    class="section section--hero hero "
-  >
-    Coming Soon...
+  <section class="section section--hero hero ">
+    <div class="hero__container">
+      <h1 class="hero__title">
+        Coming Soon...
+        <svg-icon name="thumbsup" />
+      </h1>
+
+      <div class="hero__cta">
+        <a
+          href="mailto:info@corona-service.org?subject=Corona%20Service"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kontakt
+        </a>
+      </div>
+    </div>
   </section>
 </template>
 
